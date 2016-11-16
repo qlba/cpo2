c90a79d0fb52354c4206df6aea5ebe960b249878
qlba <qlba@ya.ru>
2016-11-16 20:12:46 +0300

    -

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -38,7 +37,7 @@ app.use(function (req, res, next) {
 if (app.get('env') === 'development') {
     app.use(function (err, req, res, next) {
         res.status(err.status || 500);
-        res.render('error', {
+        res.render('engine/error', {
             message: err.message,
             error: err
         });
