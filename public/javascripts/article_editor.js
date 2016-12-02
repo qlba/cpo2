d7d31fb4a4aa28daaef6d511be2efdb45161a3ab
qlba <qlba@ya.ru>
2016-12-02 23:44:32 +0300

    Can insert, rename, delete sections now

--- public/javascripts/article_editor.js
+++ public/javascripts/article_editor.js
@@ -2,24 +2,22 @@
  * Created by qlba on 01.12.2016.
  */
 
-function initEditor(sid) {
-
+function initSectionEditor(sid)
+{
     var thatSection = document.getElementById(sid);
-    var div = thatSection.getElementsByTagName("div")[0];
-    var p = thatSection.getElementsByTagName("p")[0];
+    var previewer = thatSection.getElementsByTagName("div")[1];
 
     var form = document.createElement("form");
     form.setAttribute("method", "post");
     form.setAttribute("action", "/article-edit-section");
     form.setAttribute("class", "EditorForm");
 
-
     var field = document.createElement("textarea");
     field.setAttribute("name", "newcontent");
-    field.value = article.content.find(function(s){return s.sectId == sid;}).sectContent;
+    field.setAttribute("wrap", "soft");
+    field.value = article.content[sid].sectContent;
     field.oninput = function(){
-        p.innerHTML = markdown.toHTML(field.value);
-        field.setAttribute("rows", ((field.value.match(/\n/g) || []).length + 1).toString());
+        previewer.innerHTML = markdown.toHTML(field.value);
     };
 
     var commitButton = document.createElement("input");
@@ -51,12 +49,84 @@ function initEditor(sid) {
     form.appendChild(aid);
     form.appendChild(asid);
 
-    div.parentNode.insertBefore(form, div);
-
+    previewer.parentNode.insertBefore(form, previewer);
 
 
     removePanels();
+}
 
+function initSectionRenamer(sid)
+{
+    var thatSection = document.getElementById(sid);
+    var previewer = thatSection.getElementsByTagName("h1")[0];
+
+    var form = document.createElement("form");
+    form.setAttribute("method", "post");
+    form.setAttribute("action", "/article-rename-section");
+    form.setAttribute("class", "EditorForm");
+
+    var field = document.createElement("input");
+    field.setAttribute("name", "newname");
+    field.value = article.content[sid].sectName;
+    field.oninput = function(){
+        previewer.innerHTML = field.value;
+    };
+
+    var commitButton = document.createElement("input");
+    commitButton.setAttribute("type", "submit");
+    commitButton.setAttribute("value", "Apply changes");
+
+    var rollbackButton = document.createElement("input");
+    rollbackButton.setAttribute("type", "button");
+    rollbackButton.setAttribute("value", "Discard changes");
+    rollbackButton.onclick = function () {
+        location.reload();
+    };
+
+    var aid = document.createElement("input");
+    aid.setAttribute("type", "hidden");
+    aid.setAttribute("name", "aid");
+    aid.setAttribute("value", article._id);
+
+    var asid = document.createElement("input");
+    asid.setAttribute("type", "hidden");
+    asid.setAttribute("name", "asid");
+    asid.setAttribute("value", sid);
+
+
+
+    form.appendChild(field);
+    form.appendChild(commitButton);
+    form.appendChild(rollbackButton);
+    form.appendChild(aid);
+    form.appendChild(asid);
+
+    previewer.parentNode.insertBefore(form, previewer);
+
+
+    removePanels();
+}
+
+function initSectionDeleter(sid)
+{
+    var params = {
+        aid: article._id,
+        asid: sid
+    };
+    
+    post("/article-delete-section", params);
+}
+
+
+
+function initSectionInserter(sid)
+{
+    var params = {
+        aid: article._id,
+        asid: sid
+    };
+
+    post("/article-insert-section", params);
 }
 
 
@@ -68,4 +138,25 @@ function removePanels()
         [].forEach.call(panels, function(v) {
             v.parentNode.removeChild(v);
         });
+}
+
+function post(path, params)
+{
+    var form = document.createElement("form");
+    form.setAttribute("method", "post");
+    form.setAttribute("action", path);
+
+    for(var key in params) {
+        if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+        }
+    }
+
+    document.body.appendChild(form);
+    form.submit();
 }
--- routes/article.js
+++ routes/article.js
@@ -108,6 +108,84 @@ router.post(
     }
 );
 
+router.post(
+    "/article-rename-section",
+    function(req, res, next)
+    {
+        var updOper = {};
+        updOper["content." + req.body['asid'] + ".sectName"] = req.body['newname'];
+
+
+        db.collection('articles').updateOne({_id: ObjectID(req.body['aid'])}, {$set: updOper})
+
+            .then(function(){
+                res.redirect('back');
+            })
+
+            .catch(function(error){
+                respError(res, null, error);
+            });
+
+        // respError(res, null, new Error("Not implemented"));
+
+        // console.log(req.body);
+    }
+);
+
+router.post(
+    "/article-delete-section",
+    function(req, res, next)
+    {
+        db.collection('articles').findOne({_id: ObjectID(req.body['aid'])})
+
+            .then(function(doc){
+                doc.content.splice(req.body['asid'], 1);
+                db.collection('articles').save(doc);
+            })
+
+            .then(function(){
+                res.redirect('back');
+            })
+
+            .catch(function(error){
+                respError(res, null, error);
+            });
+
+        // respError(res, null, new Error("Not implemented"));
+
+        // console.log(req.body);
+    }
+);
+
+
+router.post(
+    "/article-insert-section",
+    function(req, res, next)
+    {
+        db.collection('articles').findOne({_id: ObjectID(req.body['aid'])})
+
+            .then(function(doc){
+                doc.content.splice(
+                    +req.body['asid'] + 1,
+                    0,
+                    {
+                        "sectName": "SECTION " + (+req.body['asid'] + 1),
+                        "sectContent": ""}
+                );
+                db.collection('articles').save(doc);
+            })
+
+            .then(function(){
+                res.redirect('back');
+            })
+
+            .catch(function(error){
+                respError(res, null, error);
+            });
+
+    }
+);
+
 
 module.exports = router;
 
